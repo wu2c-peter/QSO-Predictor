20b6ee50326e295db55df43839b40cff49017df6
wu2c-peter <peter@wu2c.net>
2026-01-26 23:52:45 -0500

    import bugfix

--- main_v2.py
+++ main_v2.py
@@ -76,7 +76,7 @@ from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QH
                              QTableView, QLabel, QHeaderView, QDockWidget,
                              QMessageBox, QProgressBar, QAbstractItemView, QFrame, QSizePolicy, 
                              QSystemTrayIcon, QMenu, QToolBar, QPushButton, QCheckBox,
-                             QStyledItemDelegate)
+                             QStyledItemDelegate, QSizePolicy)
 from PyQt6.QtCore import Qt, pyqtSignal, QTimer, QAbstractTableModel, QModelIndex, QByteArray
 from PyQt6.QtGui import QColor, QAction, QKeySequence, QFont, QIcon, QCursor, QBrush
 
